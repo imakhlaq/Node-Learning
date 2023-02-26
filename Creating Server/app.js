3f84278bcd7df6c810183ce1c8e8fa6f9e76ff42
imakhlaq <imakhlaqXD@gmail.com>
2023-02-26 20:59:51 +0530

    new imports

--- Creating Server/app.js
+++ Creating Server/app.js
@@ -1,9 +1,24 @@
 import http from "http";
+import fs from "fs";
 
 //creating server
 const server = http.createServer((req, res) => {
   //process.exit();
+  const { url, method } = req;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>EnterMessage</title></head>");
+    res.write(
+      `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body> `
+    );
+    res.write("</html>");
+    // because we have to stop from sending another req
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+  }
 });
 
 //listening for req
-server.listen(20001);
+server.listen(3001);
