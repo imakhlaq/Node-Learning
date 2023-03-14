4c2fd23ca0236bb633e711121d96694116106d2c
imakhlaq <imakhlaqXD@gmail.com>
2023-03-14 19:47:24 +0530

    css

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,19 +1,25 @@
 import express from "express";
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 //parsing body for every request
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+//staticly serving files
+app.use(express.static(__dirname, "public"));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 //handling 404
 app.use((req, res) => {
-  res.status(404).send("<h1>not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "not-Found.html"));
 });
 
 //listing
