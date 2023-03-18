9866fa3141285651aead1ac65b10094846163588
imakhlaq <imakhlaqXD@gmail.com>
2023-03-18 23:43:00 +0530

    updated markup

--- expressjs/routes/admin.js
+++ expressjs/routes/admin.js
@@ -7,13 +7,16 @@ const __dirname = path.resolve();
 //creating a routes in diff application and exporting it
 const router = express.Router();
 
+//for storing data
+export const products = [];
+
 console.log(path.join(__dirname, "views", "add-product.html"));
 router.get("/add-product", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body });
 
   res.redirect("/");
 });
--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -1,11 +1,14 @@
 import express from "express";
 import path from "path";
 
+import { products } from "./admin.js";
+
 const router = express.Router();
 
 const __dirname = path.resolve();
 
 router.get("/", (req, res, next) => {
+  console.log(products);
   res.sendFile(path.join(__dirname, "views", "shop.html"));
 });
 
