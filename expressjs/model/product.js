0beb63bd555ba238b3709ac23e39cca2efcf1a4e
imakhlaq <imakhlaqXD@gmail.com>
2023-04-15 02:00:21 +0530

    made code async

--- expressjs/model/product.js
+++ expressjs/model/product.js
@@ -1,8 +1,5 @@
-import fs from "fs";
 import path from "path";
-
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import { readFile, writeFile } from "fs/promises";
 
 //for storing data
 const __dirname = path.resolve();
@@ -13,29 +10,34 @@ export class Product {
   constructor(item) {
     this.item = item;
   }
-  save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
+  async save() {
+    let products = [];
 
+    try {
+      products = await readFile(p);
       //if their is no error the retriving data from file
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+      products = JSON.parse(products);
+      console.log(products);
+    } catch (err) {
+      console.log(products);
+    } finally {
       products.push({ title: this.item });
+    }
 
-      //saving in file
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
-    });
+    //saving in file
+    try {
+      writeFile(p, JSON.stringify(products));
+    } catch (err) {
+      console.log(err);
+    }
   }
-  static fetchAll() {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return [];
-      }
-
-      return JSON.parse(fileContent);
-    });
+  static async fetchAll() {
+    let fileContent;
+    try {
+      fileContent = await readFile(p);
+    } catch (err) {
+      return [];
+    }
+    return JSON.parse(fileContent);
   }
 }
--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -5,12 +5,11 @@ import { Product } from "../model/product.js";
 const router = express.Router();
 const __dirname = path.resolve();
 
-router.get("/",  (req, res, next) => {
+router.get("/", async (req, res, next) => {
   //sending file
   // res.sendFile(path.join(__dirname, "views", "shop.html"));
 
-  const products = Product.fetchAll();
-  console.log(products, "++++++++++++++++");
+  const products = await Product.fetchAll();
 
   //sending pug file instead of HTMl
   res.render("shop", { products, doctitle: "shop" });
